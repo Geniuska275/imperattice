81b274e22003adfcbf7309f3cf1532bfba949a95
Geniuska275 <aigbojie2020@gmail.com>
2024-04-25 08:02:34 +0100

    pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Pagination = ({imagesPerPage,totalImages,setCurrentPage}) => {
+    const [num,setNum]=useState(null)
    const paginate=(pagenumber)=>{
+     setNum(pagenumber)
     setCurrentPage(pagenumber)}
     const ImageNumbers=[]
     for(let i=1; i<=Math.ceil(totalImages/imagesPerPage);i++){
       ImageNumbers.push(i)
     }
+    const exact=ImageNumbers.filter((item)=>item ===num)
+    
+
   return (
-    <nav className='flex justify-center items-center'>
+    <nav className='flex justify-center '>
      <ul className='flex gap-2 m-4'>
-        {ImageNumbers.map(number=>(
-        <li className='border p-4 shadow rounded text-[#bf2833]'>
-            <a onClick={()=>setCurrentPage(number)}> {number}</a>
+        {ImageNumbers.map((number)=>(
+        <li className={`border p-2 shadow rounded text-[#bf2833] ${exact[0]===num ? "bg-[#bf2833]":""}`}>
+            <a onClick={()=>paginate(number)}> {number}</a>
         </li>
         ))}
      </ul>
